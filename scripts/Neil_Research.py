8629e29840f9073f899a6e2c0276f4f86db830fc
Neil Vakharia <NeilVakharia2021@u.northwestern.edu>
2020-11-10 11:30:49 -0600

    Data Collected

--- scripts/Neil_Research.py
+++ scripts/Neil_Research.py
@@ -1,53 +1,53 @@
 import WebSearcher as ws
 import os
 import csv
+import time
 
 
-# Initialize search engine crawler
-# se = ws.SearchEngine()
-# Conduct search and retrieve data
-# se.search('Prove that the intersection of (0,1/n) for all from n = 1 to inf is 0. Notice that this demonstrates that the intervals in the Nested Interval Property must be closed for the conclusion of the theorem to hold')
-# soup = ws.make_soup(se.html)
-# Parse the SERP
-# results = ws.parse_serp(soup)
-# for i in range(5):
-# print(results[i])
-
-
-def search(query):
+def search(query, file, num):
+    parent_dir = 'RawHTML/' + file
+    dir = str(num)
+    path = os.path.join(parent_dir, dir)
+    os.mkdir(path)
     se = ws.SearchEngine()
     se.search(query)
     soup = ws.make_soup(se.html)
     results = ws.parse_serp(soup)
+    se.save_serp(save_dir=path)
+    results = [dict(item, question_number=num) for item in results]
     return results
 
 
 def getQueries():
-    isFirstQuery = True
     path = 'ResearchQueries'
     from os import listdir
     from os.path import isfile, join
     onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-
-    for file in onlyfiles:
+    for k in range(12, len(onlyfiles)):
+        file = onlyfiles[k]
         file_path = path + '/' + file
+        file = file.split('.')
+        file = file[0]
         f = open(file_path, "r")
         text = f.read()
         text = text.split("\n\n")
         for i in range(len(text)):
             text[i] = text[i].replace('\n', ' ')
-            data = search(text[i])
-            copy_to_csv(isFirstQuery, data)
-            isFirstQuery = False
-        break
+            time.sleep(30)
+            data = search(text[i], file, i + 1)
+            try:
+                copy_to_csv(data)
+            except Exception as e:
+                print(e)
+                print(onlyfiles[k], print(i), print(text))
 
 
-def copy_to_csv(write_header, dict_list):
-
+def copy_to_csv(dict_list):
     keys = dict_list[0].keys()
-    with open('ResearchQueries/data.csv', 'a') as csvfile:
+    with open('data.csv', 'a') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=keys)
         writer.writeheader()
         writer.writerows(dict_list)
+
 
 getQueries()
